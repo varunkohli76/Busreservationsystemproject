fe43ae5428dadbe25e0a0b504da52f586cbabf37
varunkohli76 <varunwins123@gmail.com>
2023-11-27 22:44:23 +0530

    Third commit

--- assets/js/Home.js
+++ assets/js/Home.js
@@ -18,32 +18,35 @@
 // }
 
 let busroute = [
-    { from: "CHANDIGARH", to: "SUJANPUR H.P", Timings: "06:15AM" },
-    { from: "CHANDIGARH", to: "SUJANPUR H.P", Timings: "08:30AM" },
-    { from: "CHANDIGARH", to: "MANALI", Timings: "09:30AM" },
-    { from: "CHANDIGARH", to: "MANALI", Timings: "08:00PM" },
-    { from: "CHANDIGARH", to: "HAMIRPUR", Timings: "06:00AM" },
-    { from: "CHANDIGARH", to: "DEHLI", Timings: "05:00PM" },
-    { from: "CHANDIGARH", to: "DEHLI", Timings: "09:00PM" },
-    { from: "CHANDIGARH", to: "DEHLI", Timings: "11:30PM" },
-    { from: "CHANDIGARH", to: "UNA", Timings: "04:00AM" },
-    { from: "CHANDIGARH", to: "UNA", Timings: "10:00AM" },
-    { from: "CHANDIGARH", to: "UNA", Timings: "01:00PM" },
-    { from: "CHANDIGARH", to: "BAIZNATH", Timings: "07:22AM" },
-    { from: "CHANDIGARH", to: "BAIZNATH", Timings: "03:30PM" },
+    { from: "CHANDIGARH", to: "SUJANPUR H.P", Timings: "06:15AM", Type: "AC", Price: 435 },
+    { from: "CHANDIGARH", to: "SUJANPUR H.P", Timings: "08:30AM", Type: "ORD", Price: 385 },
+    { from: "CHANDIGARH", to: "MANALI", Timings: "09:30AM", Type: "ORD", Price: 385 },
+    { from: "CHANDIGARH", to: "MANALI", Timings: "08:00PM", Type: "AC", Price: 540 },
+    { from: "CHANDIGARH", to: "MANALI", Timings: "08:00PM", Type: "ORD", Price: 480 },
+    { from: "CHANDIGARH", to: "HAMIRPUR", Timings: "06:40AM", Type: "AC", Price: 420 },
+    { from: "CHANDIGARH", to: "DEHLI", Timings: "05:00PM", Type: "AC", Price: 400 },
+    { from: "CHANDIGARH", to: "DEHLI", Timings: "09:00PM", Type: "AC", Price: 400 },
+    { from: "CHANDIGARH", to: "DEHLI", Timings: "11:30PM", Type: "ORD", Price: 375 },
+    { from: "CHANDIGARH", to: "UNA", Timings: "04:00AM", Type: "AC", Price: 220 },
+    { from: "CHANDIGARH", to: "UNA", Timings: "10:00AM", Type: "ORD", Price: 170 },
+    { from: "CHANDIGARH", to: "UNA", Timings: "01:00PM", Type: "ORD", Price: 170 },
+    { from: "CHANDIGARH", to: "BAIZNATH", Timings: "07:22AM", Type: "AC", Price: 430 },
+    { from: "CHANDIGARH", to: "BAIZNATH", Timings: "11:30AM", Type: "AC", Price: 430 },
+    { from: "CHANDIGARH", to: "BAIZNATH", Timings: "02:00PM", Type: "ORD", Price: 395 },
+    { from: "CHANDIGARH", to: "BAIZNATH", Timings: "08:30PM", Type: "AC", Price: 430 },
 ];
 
 
 let button = document.getElementById('btn7');
-button.addEventListener("click", function (){
+button.addEventListener("click", function () {
     let start = document.getElementById('From').value;
     let destination = document.getElementById('To').value;
 
     if (start === "" && destination === "") {
         alert("Enter the first destination and last destination ");
-    } 
-    else if(start === destination){
-      alert("Same fields are not allowed please select different fields");
+    }
+    else if (start === destination) {
+        alert("Same fields are not allowed please select different fields");
     }
     let tableHTML = `
     <table class="table table-striped">
@@ -52,24 +55,28 @@ button.addEventListener("click", function (){
                 <th>FROM</th>
                 <th>TO</th>
                 <th>TIMINGS</th>
+                <th>TYPE</th>
+                <th>PRICE</th>
                 <th>BOOK</th>
             </tr>
         </thead>
         <tbody>
 `;
-    for(let i=0;i<busroute.length;i++){
-        if(busroute[i].from === start && busroute[i].to === destination){
+    for (let i = 0; i < busroute.length; i++) {
+        if (busroute[i].from === start && busroute[i].to === destination) {
             document.getElementById('storebusesdetails').innerHTML = //busroute[i].from + ' ' +'to' + busroute[i].to + busroute[i].Timings+"<br>";
-            tableHTML += `
+                tableHTML += `
             <tr>
                 <td>${busroute[i].from}</td>
                 <td>${busroute[i].to}</td>
                 <td>${busroute[i].Timings}</td>
+                <td>${busroute[i].Type}</td>
+                <td>${busroute[i].Price}</td>
                 <td><a href="assets/busseats.html" button type="submit"class="btn btn-info">BOOK</a></button></td>    
             </tr>
         `;
-    }
         }
+    }
 });
 
 
